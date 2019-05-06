d5adfde2cf79ab8a003788f767ce6cbbf7b06df7
Yazid-Krayem <ykrayyem@gmail.com>
2019-05-06 21:23:47 +0300

    router folder

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ const express     = require('express')
 const app         = express();
 const port        =  process.env.PORT || 4000
 
+//Route
+const userRoutes = require('./router/user')
+const PostRoutes = require('./router/post')
+
+
+app.use(express.json())
+
+app.use(userRoutes)
+app.use(PostRoutes)
+
+
 app.listen(port,() =>{
     console.log('server is up on ' + port);
 })
